5b395608885da4ae3e5c215ce78729c8fa8b0123
cereme(Sangyun Pae) <ceremebsy@gmail.com>
2021-03-23 22:33:41 +0900

    format code

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    semi: true,
-    trailingComma: 'es5',
-    printWidth: 120,
-    tabWidth: 2,
-    useTabs: false,
-    singleQuote: true,
-  }
+  semi: true,
+  trailingComma: 'es5',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+}
